e1d465ff884914707572e6bd7fbdd1acf764ed0b
TheHolyDenier <bloodthirsty.snow.white@gmail.com>
2020-07-20 17:36:57 +0200

    first commit

---
+++ app/src/main/java/com/example/dacaco/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dacaco.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dacaco.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
