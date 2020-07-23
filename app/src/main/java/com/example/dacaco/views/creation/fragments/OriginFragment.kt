5477b88d5c049af67a8da2d238ae9fc681f39a50
TheHolyDenier <bloodthirsty.snow.white@gmail.com>
2020-07-23 17:45:21 +0200

    binding!!

--- app/src/main/java/com/example/dacaco/views/creation/fragments/OriginFragment.kt
+++ app/src/main/java/com/example/dacaco/views/creation/fragments/OriginFragment.kt
@@ -1,33 +1,34 @@
 package com.example.dacaco.views.creation.fragments
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.ImageButton
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.dacaco.R
+import com.example.dacaco.databinding.FragmentOriginBinding
 import com.example.dacaco.models.Homeworld
 import com.example.dacaco.utils.Dice
 import com.example.dacaco.views.OriginDialogFragment
-import com.example.dacaco.views.creation.fragments.listeners.OriginClickListener
 
 
 class OriginFragment : Fragment() {
-    private lateinit var mOriginAutoCompleteTextView: AutoCompleteTextView
-    private lateinit var mOriginBtn: ImageButton
-    private lateinit var mOriginInfoBtn: ImageButton
 
+    private lateinit var binding: FragmentOriginBinding
     private lateinit var items: List<String>
+    private var world: Homeworld = Homeworld.worlds[0]
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_origin, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_origin, container, false
+        )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,55 +36,72 @@ class OriginFragment : Fragment() {
 
         items = Homeworld.worlds.map { resources.getString(it.title) }
         val adapter = ArrayAdapter(requireContext(), R.layout.item_list, items)
-        mOriginAutoCompleteTextView = view.findViewById(R.id.origin_select_world_autocomplete)
-        mOriginAutoCompleteTextView.setAdapter(adapter)
+        binding.originSelectWorldAutocomplete.setAdapter(adapter)
+        binding.originSelectWorldDice.setOnClickListener { genOriginByLuck() }
+        binding.originSelectWorldInfo.setOnClickListener { openDialogHomeworldInfo() }
+        binding.originRandomCharacteristics.setOnClickListener { showHideRandomCharacteristics() }
+        binding.originWoundsDice.setOnClickListener { setRandomWounds() }
+    }
 
-        mOriginBtn = view.findViewById(R.id.origin_select_world_dice)
-        mOriginBtn.setOnClickListener(
-            OriginClickListener(
-                this@OriginFragment
-            )
-        )
+    private fun setRandomWounds() {
+        val result: Int = Dice.throw1dN(world.wounds.diceSides)
+        binding.originWoundsInput.text = Editable.Factory.getInstance().newEditable("$result")
+        binding.originWoundsInputLayout.suffixText = "= ${world.wounds.modifier + result}"
+    }
 
-        mOriginInfoBtn = view.findViewById(R.id.origin_select_world_info)
-        mOriginInfoBtn.setOnClickListener(
-            OriginClickListener(
-                this@OriginFragment
-            )
-        )
+    private fun showHideRandomCharacteristics() {
+        val random = binding.originRandomCharacteristics.isChecked
+        binding.originCharacteristicsDice.visibility = if (random) View.VISIBLE else View.INVISIBLE
+        binding.originCharacteristicsPoints.visibility =
+            if (!random) View.VISIBLE else View.INVISIBLE
+        updateCharacteristicsInputs()
+    }
+
+    private fun updateCharacteristicsInputs() {
 
     }
 
-    fun genOriginByLuck() {
+    private fun genOriginByLuck() {
         val dice: Int = Dice.throw1dN(100)
         for ((index, world) in Homeworld.worlds.withIndex()) {
             if (world.luck >= dice) {
-                mOriginAutoCompleteTextView.setText(items[index], false)
+                binding.originSelectWorldAutocomplete.setText(items[index], false)
+                binding.world = world
                 break
             }
         }
     }
 
-    fun openDialogHomeworldInfo() {
+    private fun openDialogHomeworldInfo() {
         val fragmentManager = childFragmentManager
         val f = OriginDialogFragment()
         val args = Bundle()
         args.putInt("index", getOriginIndex())
         f.arguments = args
 
-
-
         f.show(fragmentManager, "dialog")
 
     }
 
     private fun getOriginIndex(): Int {
-        val title = mOriginAutoCompleteTextView.text
+        val title = binding.originSelectWorldAutocomplete.text
         for ((index, item) in items.withIndex()) {
             if (item.contentEquals(title)) {
                 return index
             }
         }
         return 0
+    }
+
+    fun showInfo() {
+        if (binding.originSelectWorldAutocomplete.text.toString().isNotEmpty()) {
+            binding.originLayout.visibility = View.VISIBLE
+            reloadDataHomeworld()
+        }
+    }
+
+    private fun reloadDataHomeworld() {
+        val position: Int = items.indexOf(binding.originSelectWorldAutocomplete.text.toString())
+        world = Homeworld.worlds[position]
     }
 }
---
+++ app/src/main/java/com/example/dacaco/views/creation/fragments/listeners/HomeworldTextWatcher.kt
@@ -0,0 +1,17 @@
+package com.example.dacaco.views.creation.fragments.listeners
+
+import android.text.Editable
+import android.text.TextWatcher
+import com.example.dacaco.views.creation.fragments.OriginFragment
+
+class HomeworldTextWatcher(private val fragment: OriginFragment) : TextWatcher {
+    override fun afterTextChanged(p0: Editable?) {
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        fragment.showInfo()
+    }
+}
